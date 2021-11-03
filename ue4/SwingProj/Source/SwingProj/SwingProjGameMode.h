9a86238f1e213153f99a92342989231701003eab
artem62881 <artem.m62881@gmail.com>
2021-11-03 10:16:02 +0300

    Project initial submit

---
+++ ue4/SwingProj/Source/SwingProj/SwingProjGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwingProjGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASwingProjGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASwingProjGameMode();
+};
+
+
+
